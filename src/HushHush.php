258e651fafbe9f971e3b86c8391a28beaf269371
Renaldas Tauras <renaldas.tauras@sykescottages.co.uk>
2020-07-20 16:14:43 +0100

    Stop throwing exceptions

--- src/HushHush.php
+++ src/HushHush.php
@@ -3,6 +3,7 @@
 namespace trenaldas\HushHush;
 
 use Aws\Exception\AwsException;
+use Aws\Exception\CredentialsException;
 use Aws\SecretsManager\SecretsManagerClient;
 use Exception;
 use Illuminate\Support\Facades\App;
@@ -51,12 +52,14 @@ class HushHush
         ) {
             $secret = json_decode($this->openSecret($hushHushYml['database']['environments'][App::environment()]));
 
-            if (config('hush-hush.database_option.config')) {
-                $this->useConfig($secret, $hushHushYml);
-            }
+            if ($secret) {
+                if (config('hush-hush.database_option.config')) {
+                    $this->useConfig($secret, $hushHushYml);
+                }
 
-            if (config('hush-hush.database_option.env_file')) {
-                $this->useEnvFile($secret);
+                if (config('hush-hush.database_option.env_file')) {
+                    $this->useEnvFile($secret);
+                }
             }
         }
     }
@@ -90,34 +93,12 @@ class HushHush
                     'SecretId' => $secretName,
                 ]
             );
-        } catch (AwsException $e) {
-            $error = $e->getAwsErrorCode();
-            if ($error == 'DecryptionFailureException') {
-                // Secrets Manager can't decrypt the protected secret text using the provided AWS KMS key.
-                // Handle the exception here, and/or rethrow as needed.
-                throw $e;
-            }
-            if ($error == 'InternalServiceErrorException') {
-                // An error occurred on the server side.
-                // Handle the exception here, and/or rethrow as needed.
-                throw $e;
-            }
-            if ($error == 'InvalidParameterException') {
-                // You provided an invalid value for a parameter.
-                // Handle the exception here, and/or rethrow as needed.
-                throw $e;
-            }
-            if ($error == 'InvalidRequestException') {
-                // You provided a parameter value that is not valid for the current state of the resource.
-                // Handle the exception here, and/or rethrow as needed.
-                throw $e;
-            }
-            if ($error == 'ResourceNotFoundException') {
-                // We can't find the resource that you asked for.
-                // Handle the exception here, and/or rethrow as needed.
-                throw $e;
-            }
+        } catch (CredentialsException|AwsException $e) {
+            echo 'Aws throws exception: ' . $e->getMessage();
+
+            return false;
         }
+
         // Decrypts secret using the associated KMS CMK.
         // Depending on whether the secret is a string or binary, one of these fields will be populated.
         if (isset($result['SecretString'])) {
